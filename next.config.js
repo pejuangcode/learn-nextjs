dd5ea8bb691b8c11862f5e12325a9e7d1724a981
hello.musliadi@gmail.com <hello.musliadi@gmail.com>
2023-07-12 16:12:52 +0700

    update last changes

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+        domains: ["ui-avatars.com"],
+    },
+}
 
 module.exports = nextConfig
